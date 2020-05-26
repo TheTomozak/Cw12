4a023fa3ec9f66ef1283ebb76c547d2d55b9f322
s18969 <s18969@pjwstk.edu.pl>
2020-05-26 23:51:41 +0200

    Added project finished in half

---
+++ Cw12/Controllers/DoctorsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw12.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw12.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        private readonly s18969Context _context;
+
+        public DoctorsController(s18969Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+
+            return View(_context.Doctor.ToList());
+        }
+
+        public IActionResult GetDoctors()
+        {
+            return View();
+        }
+    }
+}
---
+++ Cw12/Controllers/PatientsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw12.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cw12.Controllers
+{
+    public class PatientsController : Controller
+    {
+
+        private readonly s18969Context _context;
+
+        public PatientsController(s18969Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Patient.ToList());
+        }
+
+
+        public IActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var patient = _context.Patient.First(pat => pat.IdPatient == id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(patient);
+        }
+    }
+}
